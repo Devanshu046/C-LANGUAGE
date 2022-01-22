9d6edf43e986c0f7da52ff4060b810bf71c91ef9
Devanshu046 <83020663+Devanshu046@users.noreply.github.com>
2022-01-22 13:59:34 +0530

    Add files via upload

---
+++ &&.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int m,n;
+    printf("first=");
+    scanf("%d",&m);
+    printf("second=");
+    scanf("%d",&n);
+    if((m>35)&&(n>35))
+    {
+        printf("pass");
+    }
+    else
+    {
+        printf("fail");
+    }
+    
+}
